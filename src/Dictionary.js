6514bfd890b825717122f510b4d49519a00b950b
jakobi007 <sylwia.salapa@gmail.com>
2023-10-17 11:49:31 +0100

    Added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {}
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
   }
 
-  function handleKeywordChange(event) {
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
+
+  https: function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
   return (
