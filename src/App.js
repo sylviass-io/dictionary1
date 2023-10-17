300f2e048cfa30d9d3c22c5f8fea4220c59cb989
jakobi007 <sylwia.salapa@gmail.com>
2023-10-17 11:31:14 +0100

    Created a search engine functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header>Welcome</header>
-      <main>Dictionary App</main>
-      <footer>Coded by Sylvia SS</footer>
+      <div className="container">
+        <header>Welcome</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Sylvia SS</small>
+        </footer>
+      </div>
     </div>
   );
 }
