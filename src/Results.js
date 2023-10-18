aad03b15cc1d2290cbf7e1f9361589a216ea004c
jakobi007 <sylwia.salapa@gmail.com>
2023-10-18 10:50:39 +0100

    Added phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map((phonetics, index) => (
+          <div key={index}>
+            <Phonetics phonetics={phonetics} />
+          </div>
+        ))}
         {props.results.meanings.map((meaning, index) => (
           <div key={index}>
             <Meaning meaning={meaning} />
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="synonyms">
+      <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
